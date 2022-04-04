1904ccd315f02f82278e43b1d6be99e62ec2008f
Christine K <chknight@umich.edu>
2022-04-04 17:06:35 -0400

    Adding R script for one-way t-tests.

--- R/MechaCarChallenge.R
+++ R/MechaCarChallenge.R
@@ -14,7 +14,7 @@ suspensionCoil_df <- read.csv("../data/Suspension_Coil.csv")
 
 # 4 - Linear Regression to Predict MPG ------------------------------------
 # Print output to file instead of console
-# sink("mpg_lm_output.txt")
+sink("mpg_lm_output.txt")
 
 # Write multiple regression to determine if correlation between mpg and other metrics
 mpg_lm <- mechaCarMpg_df %>% 
@@ -23,7 +23,7 @@ mpg_lm <- mechaCarMpg_df %>%
 summary(mpg_lm)
 
 # Return to printing output to console
-# sink()
+sink()
 
 # 5 - Summary Statistics on Suspension Coils ------------------------------
 # Retrieve summary statistics for entire Suspension_Coil data set
@@ -50,7 +50,29 @@ write.csv(total_summary, "total_summary.csv", row.names = FALSE)
 write.csv(lot_summary, "lot_summary.csv", row.names = FALSE)
 
 # 6 - T-Tests on Suspension Coils -----------------------------------------
+# Print output to file instead of console
+sink("ttest_output.txt")
 
+# Determine if PSI across all manufacturing lots is statistically different
+# from the pop. mean of 1500.
+t.test(suspensionCoil_df$PSI, mu = 1500)
+
+# Create subsets of data for each lot
+Lot1_df <- suspensionCoil_df %>% filter(Manufacturing_Lot == "Lot1")
+Lot2_df <- suspensionCoil_df %>% filter(Manufacturing_Lot == "Lot2")
+Lot3_df <- suspensionCoil_df %>% filter(Manufacturing_Lot == "Lot3")
+
+# Determine if PSI of Lot 1 differs from pop. mean of 1500.
+t.test(Lot1_df$PSI, mu = 1500)
+
+# Determine if PSI of Lot 2 differs from pop. mean of 1500.
+t.test(Lot2_df$PSI, mu = 1500)
+
+# Determine if PSI of Lot 3 differs from pop. mean of 1500.
+t.test(Lot3_df$PSI, mu = 1500)
+
+# Return to printing output to console
+sink()
 
 
 
